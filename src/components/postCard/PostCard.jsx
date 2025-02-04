fd999f0408edbbe9abcc6cceaf0666a277b63fde
Md. Shahrukh Khan <shahrukhkhan3418198@gmail.com>
2025-02-04 07:05:11 +0600

    Initial commit

---
+++ src/components/postCard/PostCard.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import styles from "./postCard.module.css";
+import Image from "next/image";
+
+const PostCard = ({ post }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        {post.img && (
+          <div className={styles.imgContainer}>
+            <Image src={post.img} alt="" fill className={styles.img} />
+          </div>
+        )}
+        <span className={styles.date}>01.01.2025</span>
+      </div>
+      <div className={styles.bottom}>
+        <h1 className={styles.title}>{post.title}</h1>
+        <p className={styles.desc}>{post.body}</p>
+        <Link className={styles.link} href={`/blog/${post.slug}`}>
+          READ MORE
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
